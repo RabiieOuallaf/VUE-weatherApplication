35457e9b39d9f68957e4914e132e91cb64d14f7e
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-01-02 11:05:56 +0100

    Setting up the application and getting started

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,27 @@
     <main>
     
       <div class="search-box">
+
         <input type="text" class="search-input" placeholder="search.." />
+
       </div>
+
+      <div class="weather-wrap">
+
+        <div class="location-box">
+
+          <div class="location">Northmapton, UK</div>
+          <div class="date">Monday 2 Juanuary 2023</div>
+
+        </div>
+
+      </div>
+
+      <div class="weather-box">
+        <div class="temp">8°c</div>
+          <div class="weather">Rain</div>
+      </div>
+
 
     </main>
 
@@ -56,7 +75,7 @@ main {
   width: 100%;
   margin-bottom: 30px;
 }
-.search-box .search-bar {
+.search-box .search-input {
   display: block;
   width: 100%;
   padding: 15px;
@@ -72,7 +91,7 @@ main {
   border-radius: 0px 16px 0px 16px;
   transition: 0.4s;
 }
-.search-box .search-bar:focus {
+.search-box .search-input:focus {
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0px 16px 0px;
